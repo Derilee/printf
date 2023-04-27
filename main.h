d213d25e65da18f21bbb83b239519b9f4a5865e4
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-27 02:05:20 +0000

    updated my main header files and coded a print function

--- main.h
+++ main.h
@@ -5,10 +5,24 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-
+/** putchar and buffer */
 int _putchar(char c);
 int _buffer(char c);
 
 int _printf(const char *format, ...);
 
+/** handles the format specifies */
+int handles(const char *str, va_list list);
+int conversion_handling(const char *str, va_list list, int *i);
+
+/** print tools */
+int _strlen(const char *str);
+int print(char *str);
+
+
+/** print character */
+int printf_char(va_list list);
+
+/** print string */
+int printf_string(va_list list);
 #endif
--- printf_s.c
+++ printf_s.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * printf_string - Print string
+ * printf_string - function to Print a string
  * @list: list.
- * Return: String length.
+ * Return: length of the string
  */
 int printf_string(va_list list)
 {
