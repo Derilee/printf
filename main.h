41501170145ff035ae212b92987794399f89bf57
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-28 11:52:32 +0000

    debugging header file

--- main.h
+++ main.h
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/** main prototype */
+int _printf(const char *format, ...);
+
 /** putchar and buffer */
 int _putchar(char c);
 int _buffer(char c);
-
-int _printf(const char *format, ...);
 
 /** handles the format specifies */
 int handles(const char *str, va_list list);
@@ -25,6 +26,7 @@ int printf_char(va_list list);
 
 /** print string */
 int printf_str(va_list list);
+int printf_rev_str(va_list);
 
 /** print integers */
 int printf_int(va_list);
@@ -34,6 +36,19 @@ int printf_binary(va_list);
 
 /** print octal values */
 int printf_octal(va_list);
+
+/** print unsigned */
+int printf_unsigned(va_list);
+
+/** print hexadecimal */
+int printf_hexdec_low(va_list);
+int printf_hexdec_high(va_list);
+
+/** print pointers */
+int printf_ptr(va_list);
+
+/** print rot */
+int printf_rot(va_list);
 
 /**
  * struct _format - struct
