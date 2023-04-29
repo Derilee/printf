c53910256e3685331a6a6972fa48897f0f8b32fe
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-29 01:37:27 +0000

    debugging

---
+++ printf_hexdec.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ * print_hexadecimal_upp - Print a number in hexadecimal format
+ * @list: Number to print
+ *
+ * Return: Length of the number
+ **/
+
+int printf_hexdec_high(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = int_ascii(va_arg(list, unsigned int), 16);
+	p_buff = to_alpha(p_buff);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+
+/**
+ * _isalpha - Check for uppper and lower case
+ * @c:  The variable used for int
+ * Return: Always (0) success
+ */
+int _isalpha(int c)
+{
+
+        return (c >= 'A' && c <= 'Z');
+}
+
+/**
+ * _islower - Entry point
+ *
+ * @c: The variable
+ *
+ * Return: ALways(0) Success
+ */
+int _islower(int c)
+{
+
+        return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * *to_alpha - Change the string to uppercase
+ * @c: String
+ * Return: String uppercase
+ **/
+char *to_alpha(char *c)
+{
+        int i;
+
+        for (i = 0; c[i] != '\0'; i++)
+        {
+                if (is_lower(c[i]))
+                {
+                        c[i] = c[i] - 32;
+                }
+        }
+
+        return (c);
+}
